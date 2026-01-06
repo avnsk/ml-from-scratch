49f03dbb20117f159aa3507baf08459799ab1353
avnsk <ananay@192.168.1.216>
2026-01-06 06:00:36 +0000

    Added utils.py and modularized linear regression

---
+++ src/utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+
+# For reproducibilty purpose.
+np.random.seed(42)
+
+
+def mse(y_true, y_pred):
+    error = y_true - y_pred
+    return np.mean(error**2)
+
+
+def rmse(y_true, y_pred):
+    return np.sqrt(mse(y_true, y_pred))
+
+
+def mae(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
+
+def load_dataset(csv_path):
+    """
+    Expects CSV with columns: x1, x2, ..., y
+    """
+    df = pd.read_csv(csv_path)
+
+    X = df.drop("y", axis=1).values
+    y = df["y"].values.reshape(-1, 1)
+
+    # Add bias column
+    X_bias = np.c_[np.ones(len(X)), X]
+    return X_bias, y
+
+
+def predict(X, w):
+    return X @ w
+
+
+def generate_data(n_samples=50, n_features=3, noise_std=0.5):
+
+    X = np.random.rand(n_samples, n_features)
+    true_w = np.array([[5], [2], [-3], [1]])
+    X_bias = np.c_[np.ones(n_samples), X]
+    noise = np.random.normal(0, noise_std, size=(n_samples, 1))
+    y = X_bias @ true_w + noise
+    return X, X_bias, y, true_w
